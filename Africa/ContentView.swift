09eee11c3eee7d7890fa761894e1cb684d01566b
Sebastian Mesa <semeguze@hotmail.com>
2022-05-30 21:20:17 -0500

    Initial Commit

---
+++ Africa/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Africa
+//
+//  Created by Sebastian Mesa on 30/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
