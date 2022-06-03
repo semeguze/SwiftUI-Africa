7fef5596fc800c312c7cc443bb670492e9d3f472
Sebastian Mesa <semeguze@hotmail.com>
2022-06-03 00:05:03 -0500

    Initial commit

---
+++ Africa/Helper/VideoPlayerHelper.swift
@@ -0,0 +1,19 @@
+//
+//  VideoPlayerHelper.swift
+//  Africa
+//
+//  Created by Sebastian Mesa on 1/06/22.
+//
+
+import Foundation
+import AVKit
+
+var videoPlayer: AVPlayer?
+
+func playVideo(fileName: String, fileFormat: String) -> AVPlayer {
+  if Bundle.main.url(forResource: fileName, withExtension: fileFormat) != nil {
+    videoPlayer = AVPlayer(url: Bundle.main.url(forResource: fileName, withExtension: fileFormat)!)
+    videoPlayer?.play()
+  }
+  return videoPlayer!
+}
---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Sebastian Mesa on 30/05/22.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+  let id: Int
+  let name: String
+}
