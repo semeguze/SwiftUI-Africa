48972f40c756dee025a9b95457bd142a888c660f
Sebastian Mesa <semeguze@hotmail.com>
2022-06-03 01:14:10 -0500

    Adding toolbar grid buttons

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -10,33 +10,99 @@ import SwiftUI
 struct ContentView: View {
   // MARK: - PROPERTIES
   let animals:[Animal] = Bundle.main.decode("animals.json")
+  let haptics = UIImpactFeedbackGenerator(style: .medium)
+  
+  @State private var isGridViewActive: Bool = false
+  @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+  @State private var gridColumn: Int = 1
+  @State private var toolbarIcon: String = "square.grid.2x2"
+  
+  // MARK: - FUNCTIONS
+  func gridSwitch() {
+    gridLayout = Array(repeating: .init(.flexible()), count: gridLayout.count % 3 + 1)
+    gridColumn = gridLayout.count
+    print("Grid Number: \(gridColumn)")
+    
+    switch gridColumn {
+    case 1:
+      toolbarIcon = "square.grid.2x2"
+    case 2:
+      toolbarIcon = "square.grid.3x2"
+    case 3:
+      toolbarIcon = "rectangle.grid.1x2"
+    default:
+      toolbarIcon = "square.grid.2x2"
+    }
+  }
   
   // MARK: - BODY
   var body: some View {
     NavigationView {
-      List {
-        if #available(iOS 15.0, *) {
-          CoverImageView()
-            .frame(height: 300)
-            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+      
+      Group {
+        if !isGridViewActive {
+          List {
+            if #available(iOS 15.0, *) {
+              CoverImageView()
+                .frame(height: 300)
+                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                .listRowBackground(Color.clear)
+                .padding(.bottom, 20)
+                .listRowSeparator(.hidden)
+            } else {
+              // TODO: complete version for previous iOS versions
+            }
+            
+            ForEach(animals) { amimal in
+              NavigationLink(destination: AnimalDetailView(animal: amimal)) {
+                AnimalListItemView(animal: amimal)
+              }
+            }
             .listRowBackground(Color.clear)
-            .padding(.bottom, 20)
-            .listRowSeparator(.hidden)
+          }
         } else {
-          // TODO: complete version for previous iOS versions
-        }
-        
-        ForEach(animals) { amimal in
-          NavigationLink(destination: AnimalDetailView(animal: amimal)) {
-            AnimalListItemView(animal: amimal)
+          ScrollView(.vertical, showsIndicators: false) {
+            LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10){
+              ForEach(animals) { animal in
+                NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                  AnimalGridItemView(animal: animal)
+                }
+              }
+            }
+            .padding(10)
+            .animation(.easeIn)
           }
         }
-        .listRowBackground(Color.clear)
-        
       }
       .navigationTitle("Africa")
       .navigationBarTitleDisplayMode(.large)
       .listStyle(.plain)
+      .toolbar {
+        ToolbarItem(placement: .navigationBarTrailing) {
+          HStack(spacing: 16){
+            Button(action: {
+              print("List view is activated")
+              isGridViewActive = false
+              haptics.impactOccurred()
+            }) {
+              Image(systemName: "square.fill.text.grid.1x2")
+                .font(.title2)
+                .foregroundColor(isGridViewActive ? .primary: .accentColor)
+            }
+            
+            Button(action: {
+              print("Grid view is activated")
+              isGridViewActive = true
+              haptics.impactOccurred()
+              gridSwitch()
+            }) {
+              Image(systemName: toolbarIcon)
+                .font(.title2)
+                .foregroundColor(isGridViewActive ? .accentColor : .primary)
+            }
+          }
+        }
+      }
     }
   }
 }
